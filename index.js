546e8c40b4f901d962e0f21338c89fbaa03804a0
AzkSumWave36 <74044050+azkazunami36@users.noreply.github.com>
2022-09-10 00:28:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var iW = 0, iH = 0, dW = 0, dH = 0, ibuki, //宣言&初期化
     y = 0, x = 0, //キャラクターの位置、初期化
-    k = 15; //速度
+    k = 15, //速度
+    mouse = false, mouseX = 0, mouseY = 0; //マウスの状態
 function windowsizeGet() { dW = document.documentElement.clientWidth, dH = document.documentElement.clientHeight; }; //ウィンドウサイズ取得の関数
 function reflect() { //画像の位置を反映させる関数
     if (y > (dH - iH)) { y = dH - iH; };
@@ -10,6 +11,12 @@ function reflect() { //画像の位置を反映させる関数
     ibuki.style.top = y + "px";
     ibuki.style.left = x + "px";
 };
+function mousedown() {
+    mouse = true;
+    mouseX = event.clientX - x;
+    mouseY = event.clientY - y;
+    console.log(mouseX + "x" + mouseY);
+};
 window.onload = function () {
     windowsizeGet();
     ibuki = document.getElementById('ibuki'); //オフジェクト取得
@@ -18,7 +25,14 @@ window.onload = function () {
     console.log("Image Size" + iW + "x" + iH);
     //ウィンドウサイズが変更されたとき、以下のプログラムを実行する
     window.addEventListener('resize', () => { windowsizeGet(); reflect(); });
-
+    window.onmouseup = function () { mouse = false; };
+    window.onmousemove = function () { 
+        if (mouse == true) {
+            x = event.clientX - mouseX;
+            y = event.clientY - mouseY;
+            reflect();
+        }
+    };
     //なにかキーが押されたとき、以下のプログラムを実行する
     addEventListener("keydown", event => {
         if (event.keyCode == 37) x -= k; //「左ボタン」が押されたとき、xの値から32を引き算する
